a788559ec7bed7288bd46463bf3b74f91829d662
estellearrc <34720366+estellearrc@users.noreply.github.com>
2018-11-13 15:27:41 +0100

    Avanacement génération automatique de graphes

--- QUIZ_IA/App/Arete.cs
+++ QUIZ_IA/App/Arete.cs
@@ -8,21 +8,21 @@ namespace App
 {
     public class Arete
     {
-        public Sommet Pt1 { get; private set; }
-        public Sommet Pt2 { get; private set; }
+        public Sommet S1 { get; private set; }
+        public Sommet S2 { get; private set; }
         public int Distance { get; private set; }
-        public Arete(Sommet pt1, Sommet pt2)
+        public Arete(Sommet s1, Sommet s2)
         {
-            Pt1 = pt1;
-            Pt2 = pt2;
+            S1 = s1;
+            S2 = s2;
             Distance = CalculeDistance();
         }
         public int CalculeDistance()
         {
-            float x1 = Pt1.Pt.X;
-            float x2 = Pt2.Pt.X;
-            float y1 = Pt1.Pt.Y;
-            float y2 = Pt2.Pt.Y;
+            float x1 = S1.Pt.X;
+            float x2 = S2.Pt.X;
+            float y1 = S1.Pt.Y;
+            float y2 = S2.Pt.Y;
             return (int)Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
         }
         public override string ToString()
